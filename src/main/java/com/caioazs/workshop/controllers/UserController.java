80e78e338a0708a385ea4467ef0ee9f84081bbd9
CaioAzs <souzascaio23cs@gmail.com>
2023-03-10 12:30:04 -0300

    put user

--- src/main/java/com/caioazs/workshop/controllers/UserController.java
+++ src/main/java/com/caioazs/workshop/controllers/UserController.java
@@ -10,6 +10,7 @@ import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -45,13 +46,20 @@ public class UserController {
 		user = userService.insert(user);
         URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(user.getId()).toUri();
 		return ResponseEntity.created(uri).build();
-
     }
 
     //Nothing will be returned on body, just the No Content status
     @DeleteMapping(value = "/{id}")
     public ResponseEntity<Void> delete(@PathVariable String id){
         userService.delete(id);
+        return ResponseEntity.noContent().build();
+    }
+
+    @PutMapping(value = "/{id}")
+    public ResponseEntity<Void> update(@PathVariable String id, @RequestBody UserDTO userDto){
+        User user = userService.fromDTO(userDto);
+        user.setId(id);
+		user = userService.update(user);
         return ResponseEntity.noContent().build();
     }
 }
