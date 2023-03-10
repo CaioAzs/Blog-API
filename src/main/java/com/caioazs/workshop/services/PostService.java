d9d04aef09d1e2c9c20b836d91d4b9e34777cec4
CaioAzs <souzascaio23cs@gmail.com>
2023-03-10 13:57:53 -0300

    Added endpoint for post title search

--- src/main/java/com/caioazs/workshop/services/PostService.java
+++ src/main/java/com/caioazs/workshop/services/PostService.java
@@ -1,5 +1,6 @@
 package com.caioazs.workshop.services;
 
+import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -18,4 +19,9 @@ public class PostService {
         Optional<Post> post = postRepository.findById(id);
         return post.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
     }
+
+    public List<Post> findByTitle(String title){
+        return postRepository.findByTitleContainingIgnoreCase(title);
+    }
+
 }
